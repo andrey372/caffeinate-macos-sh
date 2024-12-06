61d22d12b454a3375d5722e17414b828bdfaf471
Andrew K <bluer.mullion.0h@icloud.com>
2024-12-05 23:44:53 -0600

    Update v2 caffeinate_timer.sh

--- caffeinate_timer.sh
+++ caffeinate_timer.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 # Set the duration for which the lid-sleep should be deactivated (in seconds)
-# 3600 sec = 1 hr
+# 3600 sec = 60 mins
 DURATION=3600
+
+# Function to restore sleep settings
+restore_settings() {
+    sudo pmset -b sleep 1
+    sudo pmset -b disablesleep 0
+    echo "Lid-sleep has been reactivated."
+    exit
+}
+
+# Trap to catch interrupt signals (like Ctrl+C)
+trap restore_settings SIGINT SIGTERM
 
 # Deactivate lid-sleep
 sudo pmset -b sleep 0
 sudo pmset -b disablesleep 1
 
-echo "Lid-sleep has been deactivated for $DURATION seconds."
+echo "Lid-sleep has been deactivated for $DURATION seconds. Press Ctrl+C to cancel early."
 
-# Wait for the specified duration
-sleep $DURATION
+# Wait for the specified duration or until interrupted
+for ((i=0; i<DURATION; i++)); do
+    sleep 1
+done
 
-# Reactivate lid-sleep
-sudo pmset -b sleep 1
-sudo pmset -b disablesleep 0
-
-echo "Lid-sleep has been reactivated."
+# If duration ends without interruption
+restore_settings
